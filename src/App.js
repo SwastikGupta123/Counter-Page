4a62c70cb4832a8633e18e544ce35f855c4f5aca
SwastikGupta123 <94752250+SwastikGupta123@users.noreply.github.com>
2023-01-31 21:26:32 +0530

    kbkjb

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import './App.css';
-// import { useState } from 'react';
-// import Welcome from "./Components/Welcome";
 import Counter from './Components/Counter';
 function App() {
-  // const [text,setText] = useState("");
-
-// const handleOnChanges = (event) => {
-//   setText(event.target.value);
-// }
   return (
     <>
-    {/* <p>Enter your Name <input type="text" value={text} onChange={handleOnChanges}/>
-    </p>
-    <Welcome name= {text}/> */}
     <Counter/>
     </>
   );
